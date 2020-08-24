e05691c431339f7e52f873c8b0d0eccb1c1d2732
Zaeem Zahid <zaeem@datumbrain.com>
2020-08-24 20:40:50 +0500

    [DBD-88] Added Comments in Scala Code

--- src/main/scala/org/datumbrain/TutorialApp.scala
+++ src/main/scala/org/datumbrain/TutorialApp.scala
@@ -10,29 +10,44 @@ import scala.scalajs.js.annotation.JSExportTopLevel
 object TutorialApp{
   def main(args: Array[String]): Unit = {
     println("Hello World")
+    //The Whole Robot Node
     val robot = document.getElementById("android")
+    //Head of the bot
     val head = document.getElementById("head")
+    //torso of bot
     val torso = document.getElementById("torso")
 
+    //Change Eyes Color on clicking on head
     head.addEventListener("click", { (e: dom.MouseEvent) =>
       changeEyesColor()
     })
+    //Change Arms Color on clicking on torso
     torso.addEventListener("click", { (e: dom.MouseEvent) =>
       println("here")
       changeArmsColor()
     })
   }
+  
+  //function to change color of eyes
   @JSExportTopLevel("changeEyesColor")
   def changeEyesColor() = {
     document.getElementById("left_eye").asInstanceOf[HTMLElement].classList.toggle("red_eye")
     document.getElementById("right_eye").asInstanceOf[HTMLElement].classList.toggle("red_eye")
   }
+
+  //function to change color of eyes
   @JSExportTopLevel("changeArmsColor")
   def changeArmsColor() = {
     document.getElementById("left_arm").asInstanceOf[HTMLElement].classList.toggle("red_arm")
     document.getElementById("right_arm").asInstanceOf[HTMLElement].classList.toggle("red_arm")
   }
 
+
+  /*
+  *Function to Append Paragraph within a DOM Element
+  *@param targetNode: The node to append Paragraph inside
+  *@param text: Text to be included inside Paragraph
+  */
   def appendPar(targetNode: dom.Node, text: String): Unit ={
     val parNode = document.createElement("p")
     parNode.textContent = text
